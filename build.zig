f41d302a378277d76c3736f9ad5f6d04523bce9e
Lawrence Chen <lawrencecchen@users.noreply.github.com>
2026-03-25 14:52:03 -0700

    Remove unused libvt_step (upstream changed install target)

--- build.zig
+++ build.zig
@@ -35,7 +35,6 @@ pub fn build(b: *std.Build) !void {
 
     // All our steps which we'll hook up later. The steps are shown
     // up here just so that they are more self-documenting.
-    const libvt_step = b.step("lib-vt", "Build libghostty-vt");
     const cli_helper_step = b.step("cli-helper", "Build the Ghostty CLI helper");
     const run_step = b.step("run", "Run the app");
     const run_valgrind_step = b.step(
